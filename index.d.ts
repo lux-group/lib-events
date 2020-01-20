a2cc14d0a9ad7a88d22d82905cd05a789e0c36a7
Rufus Post <rufuspost@gmail.com>
2020-01-21 07:59:21 +1100

    fix syntax errors

--- index.d.ts
+++ index.d.ts
@@ -9,45 +9,45 @@ declare module "@luxuryescapes/lib-events" {
 
   function poll(processMessage: Function): Promise<void>;
 
-  const ORDER_PENDING string;
-  const ORDER_COMPLETED string;
-  const ORDER_CANCELLED string;
-  const ORDER_REFUNDED string;
-  const ORDER_ABANDONED string;
-  const ORDER_AWAITING_PAYMENT string;
-  const ORDER_AWAITING_PURCHASE string;
-  const ORDER_NEEDS_ATTENTION string;
-  const ORDER_PAYMENT_FAILED string;
-  const ORDER_TOUCH string;
+  const ORDER_PENDING: string;
+  const ORDER_COMPLETED: string;
+  const ORDER_CANCELLED: string;
+  const ORDER_REFUNDED: string;
+  const ORDER_ABANDONED: string;
+  const ORDER_AWAITING_PAYMENT: string;
+  const ORDER_AWAITING_PURCHASE: string;
+  const ORDER_NEEDS_ATTENTION: string;
+  const ORDER_PAYMENT_FAILED: string;
+  const ORDER_TOUCH: string;
 
-  const ORDERS_CHECKSUM string;
-  const ORDERS_CHECKSUM_ERROR string;
+  const ORDERS_CHECKSUM: string;
+  const ORDERS_CHECKSUM_ERROR: string;
 
-  const ORDER_ITEM_CREATED string;
-  const ORDER_ITEM_COMPLETED string;
-  const ORDER_ITEM_AWAITING_DATES string;
-  const ORDER_ITEM_FAILED string;
-  const ORDER_ITEM_CANCELLED string;
-  const ORDER_ITEM_TOUCH string;
-  const ORDER_ITEM_CHANGE_DATES string;
-  const ORDER_ITEM_DELETE_RESERVATION string;
-  const ORDER_ITEM_UPDATE_RESERVATION string;
+  const ORDER_ITEM_CREATED: string;
+  const ORDER_ITEM_COMPLETED: string;
+  const ORDER_ITEM_AWAITING_DATES: string;
+  const ORDER_ITEM_FAILED: string;
+  const ORDER_ITEM_CANCELLED: string;
+  const ORDER_ITEM_TOUCH: string;
+  const ORDER_ITEM_CHANGE_DATES: string;
+  const ORDER_ITEM_DELETE_RESERVATION: string;
+  const ORDER_ITEM_UPDATE_RESERVATION: string;
 
-  const ORDER_ITEMS_CHECKSUM string;
-  const ORDER_ITEMS_CHECKSUM_ERROR string;
+  const ORDER_ITEMS_CHECKSUM: string;
+  const ORDER_ITEMS_CHECKSUM_ERROR: string;
 
-  const OFFER_UPDATE string;
+  const OFFER_UPDATE: string;
 
-  const PROPERTY_UPDATE string;
-  const PROPERTY_DELETE string;
+  const PROPERTY_UPDATE: string;
+  const PROPERTY_DELETE: string;
 
-  const VOUCHER_UPDATE string;
+  const VOUCHER_UPDATE: string;
 
-  const TOUR_UPDATE string;
-  const TOUR_DELETE string;
+  const TOUR_UPDATE: string;
+  const TOUR_DELETE: string;
 
-  const FLIGHT_ITEM_CREATED string;
-  const FLIGHT_ITEM_COMPLETED string;
-  const FLIGHT_ITEM_FAILED string;
-  const FLIGHT_ITEM_CANCELLED string;
+  const FLIGHT_ITEM_CREATED: string;
+  const FLIGHT_ITEM_COMPLETED: string;
+  const FLIGHT_ITEM_FAILED: string;
+  const FLIGHT_ITEM_CANCELLED: string;
 }
