4d8841bd0e01145cb67eb0af04b4fb5b4661fd74
Shawn Li <shawn.li@luxuryescapes.com>
2021-03-23 15:20:49 +1100

    add event RATE_AVAILABILITY_UPDATE (#39)
    * add event RATE_AVAILABILITY_UPDATE
    
    * bump version

--- index.d.ts
+++ index.d.ts
@@ -57,6 +57,7 @@ declare module "@luxuryescapes/lib-events" {
   const PROPERTY_DELETE: string;
 
   const ROOM_AVAILABILITY_UPDATE: string;
+  const RATE_AVAILABILITY_UPDATE: string;
 
   const HOTEL_RESERVATION_SITEMINDER_ERROR: string;
   const HOTEL_RESERVATION_TRAVELCLICK_ERROR: string;
