83e0b3b2ed5a9cdbe1e82f5a3050b7c56b1eb993
Rufus Post <rufuspost@gmail.com>
2023-07-07 11:38:23 +1200

    use sdk v3 (#106)

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,173 +4,181 @@ import {
   InvalidEventTypeError,
   InvalidFIFOMessageError,
   Events,
-} from './index';
+} from "./index";
 
-import { encodeJson } from './base64';
+import { encodeJson } from "./base64";
 
 const publisher = createPublisher({
-  accessKeyId: 'key',
-  secretAccessKey: 'secret',
-  region: 'ap-southeast-2',
-  topic: 'my-sns-topic',
-  apiHost: 'https://our-api.com'
-})
+  accessKeyId: "key",
+  secretAccessKey: "secret",
+  region: "ap-southeast-2",
+  topic: "my-sns-topic",
+  apiHost: "https://our-api.com",
+});
 
 const publisherFIFO = createPublisher({
-  accessKeyId: 'key',
-  secretAccessKey: 'secret',
-  region: 'ap-southeast-2',
-  topic: 'my-sns-topic.fifo',
-  apiHost: 'https://our-api.com'
-})
+  accessKeyId: "key",
+  secretAccessKey: "secret",
+  region: "ap-southeast-2",
+  topic: "my-sns-topic.fifo",
+  apiHost: "https://our-api.com",
+});
 
 const consumer = createConsumer({
-  accessKeyId: 'key',
-  secretAccessKey: 'secret',
-  region: 'ap-southeast-2',
-  queueUrl: 'https://sqs.ap-southeast-2.amazonaws.com/1234/my-sqs-name'
-})
+  accessKeyId: "key",
+  secretAccessKey: "secret",
+  region: "ap-southeast-2",
+  queueUrl: "https://sqs.ap-southeast-2.amazonaws.com/1234/my-sqs-name",
+});
 
 describe("index", () => {
   describe("poll", () => {
-    it('should have queue poll fun', function() {
-      expect(consumer.poll).toBeDefined()
+    it("should have queue poll fun", function () {
+      expect(consumer.poll).toBeDefined();
     });
-  })
+  });
 
   describe("dispatch", () => {
-    it('should throw error if invalid type', function() {
+    it("should throw error if invalid type", function () {
       const fun = () => {
         publisher.dispatch({
-          type: 'NA',
-          uri: '/api',
+          type: "NA",
+          uri: "/api",
           checksum: 1,
-          source: 'test',
-          message: 'test'
-        })
-      }
+          source: "test",
+          message: "test",
+        });
+      };
 
       const error = new InvalidEventTypeError("invalid event type 'NA'");
 
-      expect(fun).toThrow(error)
+      expect(fun).toThrow(error);
     });
 
-    it('should throw error if no transactionId for fifo message', function() {
+    it("should throw error if no transactionId for fifo message", function () {
       const fun = () => {
         publisherFIFO.dispatch({
           type: Events.ORDER_PENDING,
-          uri: '/api',
+          uri: "/api",
           checksum: 1,
-          source: 'test',
-          message: 'test',
-          groupId: '123',
-        })
-      }
+          source: "test",
+          message: "test",
+          groupId: "123",
+        });
+      };
 
-      const error = new InvalidFIFOMessageError('transactionId is required for FIFO messages');
+      const error = new InvalidFIFOMessageError(
+        "transactionId is required for FIFO messages"
+      );
 
-      expect(fun).toThrow(error)
+      expect(fun).toThrow(error);
     });
 
-    it('should throw error if no groupId for fifo message', function() {
+    it("should throw error if no groupId for fifo message", function () {
       const fun = () => {
         publisherFIFO.dispatch({
           type: Events.ORDER_PENDING,
-          uri: '/api',
+          uri: "/api",
           checksum: 1,
-          source: 'test',
-          message: 'test',
-          transactionId: '123'
-        })
-      }
+          source: "test",
+          message: "test",
+          transactionId: "123",
+        });
+      };
 
-      const error = new InvalidFIFOMessageError('groupId is required for FIFO messages');
+      const error = new InvalidFIFOMessageError(
+        "groupId is required for FIFO messages"
+      );
 
-      expect(fun).toThrow(error)
+      expect(fun).toThrow(error);
     });
   });
 
   describe("attributes", () => {
-    it('should pluck the message attributes', function() {
+    it("should pluck the message attributes", function () {
       const attributes = {
-        source: 'service',
-        message: 'message',
-        type: 'ORDERS_CHECKSUM',
-        checksum: 1
-      }
+        source: "service",
+        message: "message",
+        type: "ORDERS_CHECKSUM",
+        checksum: 1,
+      };
 
       const body = JSON.stringify({
-        Message: 'message',
+        Message: "message",
         MessageAttributes: {
           source: {
-            Value: 'service'
+            Value: "service",
           },
           type: {
-            Value: 'ORDERS_CHECKSUM'
+            Value: "ORDERS_CHECKSUM",
           },
           checksum: {
-            Value: 1
-          }
-        }
+            Value: 1,
+          },
+        },
       });
 
-      expect(consumer.getAttributes(body)).toEqual(attributes)
+      expect(consumer.getAttributes(body)).toEqual(attributes);
     });
 
-    it('should get json', function() {
+    it("should get json", function () {
       const attributes = {
-        source: 'service',
-        message: 'message',
-        type: 'ORDERS_CHECKSUM',
+        source: "service",
+        message: "message",
+        type: "ORDERS_CHECKSUM",
         checksum: 1,
-        json: {"name":"Beckie","age":28,"location":{"country": "Finland"}}
-      }
+        json: { name: "Beckie", age: 28, location: { country: "Finland" } },
+      };
 
       const body = JSON.stringify({
-        Message: 'message',
+        Message: "message",
         MessageAttributes: {
           source: {
-            Value: 'service'
+            Value: "service",
           },
           type: {
-            Value: 'ORDERS_CHECKSUM'
+            Value: "ORDERS_CHECKSUM",
           },
           checksum: {
-            Value: 1
+            Value: 1,
           },
           json: {
-            Value: encodeJson({"name":"Beckie","age":28,"location":{"country": "Finland"}})
-          }
-        }
+            Value: encodeJson({
+              name: "Beckie",
+              age: 28,
+              location: { country: "Finland" },
+            }),
+          },
+        },
       });
 
-      expect(consumer.getAttributes(body)).toEqual(attributes)
+      expect(consumer.getAttributes(body)).toEqual(attributes);
     });
 
-    it('should pluck the message attributes without id', function() {
+    it("should pluck the message attributes without id", function () {
       const attributes = {
-        source: 'service',
-        message: 'message',
-        type: 'ORDERS_CHECKSUM',
-        checksum: 1
-      }
+        source: "service",
+        message: "message",
+        type: "ORDERS_CHECKSUM",
+        checksum: 1,
+      };
 
       const body = JSON.stringify({
-        Message: 'message',
+        Message: "message",
         MessageAttributes: {
           source: {
-            Value: 'service'
+            Value: "service",
           },
           type: {
-            Value: 'ORDERS_CHECKSUM'
+            Value: "ORDERS_CHECKSUM",
           },
           checksum: {
-            Value: 1
-          }
-        }
+            Value: 1,
+          },
+        },
       });
 
-      expect(consumer.getAttributes(body)).toEqual(attributes)
+      expect(consumer.getAttributes(body)).toEqual(attributes);
     });
   });
 });
